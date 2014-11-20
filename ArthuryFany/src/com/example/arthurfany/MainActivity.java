c0ef0f5df1351443ea2eafa38b9c1a00069936e1
arturocercadillosanchez@gmail.com <AlumnoT@192.168.21.66>
2014-11-20 20:14:16 +0100

    hola

--- ArthuryFany/src/com/example/arthurfany/MainActivity.java
+++ ArthuryFany/src/com/example/arthurfany/MainActivity.java
@@ -11,7 +11,12 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-
+/**
+ * 
+ * @author Estefanía
+ * @author Arturo
+ * 
+ */
 public class MainActivity extends ListActivity {
 	protected String [] array;
     @Override
